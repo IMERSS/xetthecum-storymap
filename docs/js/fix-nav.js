60a8b529b4a5c9e92626d3117b454c8a269582e1
Antranig Basman <amb26ohloh@ponder.org.uk>
2024-12-10 23:34:11 +0000

    Fixed erroneous display of links in breadcrumbs

--- docs/js/fix-nav.js
+++ docs/js/fix-nav.js
@@ -11,13 +11,28 @@ if (breadholder) {
     const navholder = document.querySelector(".navbar-nav");
     const navLinks = [...navholder.querySelectorAll(".nav-item .nav-link")];
 
+    let foundNavInCrumbs = -1;
+
     navLinks.forEach(navLink => {
         const linkText = navLink.innerText.trim();
-        const inCrumbs = texts.includes(linkText);
-        if (inCrumbs) {
+        const navInCrumbs = texts.indexOf(linkText);
+        if (navInCrumbs !== -1) {
             // Status taken from https://github.com/quarto-dev/quarto-cli/blob/238afdba64f60e93970b985bfb222413f0d46811/src/project/types/website/website-navigation.ts#L565
             navLink.classList.add("active");
             navLink.setAttribute("aria-current", "page");
+            foundNavInCrumbs = navInCrumbs;
         }
     });
+
+    if (foundNavInCrumbs !== -1) {
+        const toDelinks = breadcrumbs.slice(foundNavInCrumbs);
+        toDelinks.forEach(toDelink => {
+            const span = document.createElement("span");
+            span.innerText = toDelink.innerText;
+            const parent = toDelink.parentNode;
+            parent.insertBefore(span, toDelink);
+            parent.removeChild(toDelink);
+        });
+    }
+
 }
